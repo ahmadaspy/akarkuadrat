b084af1c1d0e775e755761a7c3e6b0298dc2384b
ahmadaspy <aspy98gokils@gmail.com>
2021-03-02 22:48:05 +0800

    secure asset change

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -13,12 +13,12 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> --}}
-    <link href="{{asset('/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
     @yield('script')
 
