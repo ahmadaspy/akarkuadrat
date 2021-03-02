63f33d8a208aaa7e326339a863e105b9bbd45d5d
ahmadaspy <aspy98gokils@gmail.com>
2021-03-02 23:15:39 +0800

    change font awesome cdn

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -14,11 +14,12 @@
 
     <!-- Custom fonts for this template-->
     <link href="{{secure_asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> --}}
-    <link href="{{secure_asset('/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
     @yield('script')
 
